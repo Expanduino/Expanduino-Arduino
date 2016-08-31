b45a2eb268b2da14124051e947b0095b70adb520
Paulo Costa <paulo.costa@geofusion.com.br>
2016-08-30 23:55:57 -0300

    Added support for GPIO Keypads

---
+++ src/expanduino/arduino/keypad-linux-input.cpp
@@ -0,0 +1,111 @@
+#include "keypad-linux-input.h"
+#include <Arduino.h>
+
+ExpanduinoSubdeviceKeypadLinuxInputArduino::ExpanduinoSubdeviceKeypadLinuxInputArduino(Expanduino& container, const char* name, const char* shortName, const LinuxInputId &linuxInputId, int* rows, int numRows, int* cols, int numCols, ArduinoKeypadLinuxInputComponent* components)
+: ExpanduinoSubdeviceLinuxInput(container, name, shortName),
+  linuxInputId(linuxInputId),
+  rows(rows),
+  numRows(numRows),
+  cols(cols),
+  numCols(numCols),
+  components(components)
+{
+}
+
+void ExpanduinoSubdeviceKeypadLinuxInputArduino::begin() {
+  for (int i=0; i<numRows*numCols; i++) {
+    components[i].actualValue = 0;
+    components[i].knownValue = 0;
+  }
+  for (int row=0; row<numRows; row++) {
+    pinMode(rows[row], INPUT_PULLUP);
+  }
+  for (int col=0; col<numCols; col++) {
+    pinMode(cols[col], INPUT_PULLUP);
+  }
+  currentRow = 0;
+  rowEnabled = false;
+}
+
+void ExpanduinoSubdeviceKeypadLinuxInputArduino::end() {
+  for (uint8_t i=0; i<numRows*numCols; i++) {
+    components[i].actualValue = 0;
+    components[i].knownValue = 0;
+  }
+  for (uint8_t row=0; row<numRows; row++) {
+    pinMode(rows[row], INPUT);
+  }
+  for (uint8_t col=0; col<numCols; col++) {
+    pinMode(cols[col], INPUT);
+  }
+}
+
+void ExpanduinoSubdeviceKeypadLinuxInputArduino::pool() {
+  ArduinoKeypadLinuxInputComponent* comp=&components[currentRow*numCols];
+  if (!rowEnabled) {
+    pinMode(rows[currentRow], OUTPUT);
+    digitalWrite(rows[currentRow], LOW);    
+    rowEnabled = true;
+  } else {
+    bool interrupt = false;
+    for (uint8_t col=0; col<numCols; col++, comp++) {
+      comp->actualValue = (digitalRead(cols[col]) == LOW);
+      if (comp->actualValue != comp->knownValue) {
+        interrupt = true;
+      }
+    }
+    
+    pinMode(rows[currentRow], INPUT_PULLUP);
+    rowEnabled = false;
+    currentRow++;
+    if (currentRow == numRows) {
+      currentRow = 0;
+    }
+
+    if (interrupt) {
+      noInterrupts();
+      requestInterruption();
+      interrupts();
+    }
+  }
+}
+
+const LinuxInputId& ExpanduinoSubdeviceKeypadLinuxInputArduino::getLinuxInputId() {
+  return linuxInputId;
+}
+
+uint8_t ExpanduinoSubdeviceKeypadLinuxInputArduino::getNumComponents() {
+  return numRows * numCols;
+}
+
+LinuxInputComponentType ExpanduinoSubdeviceKeypadLinuxInputArduino::getComponentType(uint8_t componentNum) {
+  return components[componentNum].type;
+}
+
+int32_t ExpanduinoSubdeviceKeypadLinuxInputArduino::getValue(uint8_t componentNum) {
+  ArduinoKeypadLinuxInputComponent& comp = components[componentNum];
+  comp.knownValue = comp.actualValue;
+  return comp.knownValue;
+}
+
+void ExpanduinoSubdeviceKeypadLinuxInputArduino::getInterruptionReport(LinuxInputStateChange* &valuesArray, uint8_t &valuesArrayLength) {
+  valuesArray = (LinuxInputStateChange*)malloc(this->getNumComponents() * sizeof(LinuxInputStateChange));
+  for (uint8_t componentNum=0; componentNum<this->getNumComponents(); componentNum++) {
+    ArduinoKeypadLinuxInputComponent& comp = components[componentNum];
+    if (comp.knownValue != comp.actualValue) {
+      comp.knownValue = comp.actualValue;
+      valuesArray[valuesArrayLength].component = componentNum;
+      valuesArray[valuesArrayLength].value = comp.knownValue ? 1 : 0;
+      valuesArrayLength++;
+    }
+  }
+}
+
+//Not applicable on Keypads
+LinuxInputAbsoluteComponentConfig ExpanduinoSubdeviceKeypadLinuxInputArduino::getAbsoluteComponentConfig(uint8_t componentNum) {
+  return LinuxInputAbsoluteComponentConfig();
+}
+
+void ExpanduinoSubdeviceKeypadLinuxInputArduino::setValue(uint8_t componentNum, int32_t value) {
+}
+
---
+++ src/expanduino/arduino/keypad-linux-input.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../classes/linux-input.h"
+
+typedef struct {
+  LinuxInputComponentType type;
+  bool actualValue;
+  bool knownValue;
+} ArduinoKeypadLinuxInputComponent;
+
+class ExpanduinoSubdeviceKeypadLinuxInputArduino : public ExpanduinoSubdeviceLinuxInput {
+  LinuxInputId linuxInputId;
+  int* rows;
+  int numRows;
+  int* cols;
+  int numCols;
+  ArduinoKeypadLinuxInputComponent* components;
+  uint8_t currentRow;
+  bool rowEnabled;
+public:
+  ExpanduinoSubdeviceKeypadLinuxInputArduino(Expanduino& container, const char* name, const char* shortName, const LinuxInputId &linuxInputId, int* rows, int numRows, int* cols, int numCols, ArduinoKeypadLinuxInputComponent* components);
+  virtual void begin();
+  virtual void end();
+  virtual void pool();
+  virtual const LinuxInputId& getLinuxInputId();
+  virtual uint8_t getNumComponents();
+  virtual LinuxInputComponentType getComponentType(uint8_t componentNum);
+  virtual LinuxInputAbsoluteComponentConfig getAbsoluteComponentConfig(uint8_t componentNum);
+  virtual int32_t getValue(uint8_t componentNum);
+  virtual void setValue(uint8_t componentNum, int32_t value);
+  virtual void getInterruptionReport(LinuxInputStateChange* &valuesArray, uint8_t &valuesArrayLength);
+};
